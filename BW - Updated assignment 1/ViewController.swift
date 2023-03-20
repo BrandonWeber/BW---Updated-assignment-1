e3b2464c8b7aa90b9f805adb3c2c371732642162
Brandon Weber <bweber425@gmail.com>
2023-03-20 13:55:17 -0400

    Initial Commit

---
+++ BW - Updated assignment 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BW - Updated assignment 1
+//
+//  Created by Brandon Weber on 3/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
